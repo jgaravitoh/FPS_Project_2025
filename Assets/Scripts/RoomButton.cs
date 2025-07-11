3ebdfd0c4e2998fd386ffc565d47a20b581e8d70
Juan Garavito <jgaravitoh@unal.edu.co>
2025-07-11 17:01:18 -0500

    feat: ✨ Added Room Browsing, Nicknames and Starting the game from inside a lobby

---
+++ Assets/Scripts/RoomButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+using Photon.Realtime;
+public class RoomButton : MonoBehaviour
+{
+    public TMP_Text buttonText; 
+    private RoomInfo roomInfo;
+
+    public void SetButtonDetails(RoomInfo inputInfo)
+    {
+        roomInfo = inputInfo;
+        buttonText.text = roomInfo.Name;
+    }
+    public void OpenRoom()
+    {
+        Launcher.instance.JoinRoom(roomInfo);
+    }
+}
